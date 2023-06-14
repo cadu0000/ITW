20fcc2a6686d574bc453907d12abdaa7fc9f0241
cadu0000 <edukadu1000@gmail.com>
2023-06-14 16:21:03 -0300

    adicionando base do js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-var submitButton = document.getElementById("submit_button");
+document.addEventListener("DOMContentLoaded", function() {
+    const submitButton = document.getElementById("submit");
+    const resultadoDiv = document.getElementById("resultado");
+
+    submitButton.addEventListener("click", function() {
+        const inputReal = document.getElementById("real");
+        const valorReal = parseFloat(inputReal.value);
+        const valorDolar = valorReal * 5.0;
+
+        resultadoDiv.innerHTML = `${valorReal.toFixed(2)} reais em dólar é igual a: ${valorDolar.toFixed(2)} dólares`;
+    });
+});
